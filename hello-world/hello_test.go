c29fc6ff78b6a405d36a65a3f717a766461a6961
João Gomes <linharesgomes.joao@gmail.com>
2023-11-12 14:24:20 +0000

    hello-world: test for hello function in spanish

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,15 +4,22 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("John")
+		got := Hello("John", "")
 		want := "Hello, John!"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Mariana", "Spanish")
+		want := "Hola, Mariana!"
 
 		assertCorrectMessage(t, got, want)
 	})
