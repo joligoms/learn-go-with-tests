a3978e89e9e49ef24b8d0b1d5131de1aa29aa493
João Gomes <linharesgomes.joao@gmail.com>
2023-11-12 16:51:52 +0000

    iteration: implement benchmark

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,8 @@
 package iteration
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
@@ -8,5 +10,11 @@ func TestRepeat(t *testing.T) {
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
 	}
 }
