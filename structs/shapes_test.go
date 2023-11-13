a1d850a83abc974f8550c1c826a53430f03dca4f
João L. Gomes <linharesgomes.joao@gmail.com>
2023-11-13 21:55:27 +0000

    structs: add test for Area func

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -6,6 +6,19 @@ func TestPerimeter(t *testing.T) {
 	got := Perimeter(10.0, 10.0)
 	want := 40.0
 
+	assertTest(t, got, want)
+}
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 6.0)
+	want := 72.0
+
+	assertTest(t, got, want)
+}
+
+func assertTest(t testing.TB, got, want float64) {
+	t.Helper()
+
 	if got != want {
 		t.Errorf("got %.2f, want %2.f", got, want)
 	}
