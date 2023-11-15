8b5f080ee97676536a0827d1ad5ca5a190a39e60
João Gomes <linharesgomes.joao@gmail.com>
2023-11-15 19:28:34 +0000

    pointers: refactor tests with assertBalance helper

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,32 +5,27 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		assertCorrect(t, got, want)
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		assertCorrect(t, got, want)
+		assertBalance(t, wallet, Bitcoin(10))
 	})
-}
-
-func assertCorrect(t testing.TB, got, want Bitcoin) {
-	t.Helper()
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
 }
