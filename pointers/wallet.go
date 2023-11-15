1b2bb08d035b49c670b8c5440fc980e166e04b09
João Gomes <linharesgomes.joao@gmail.com>
2023-11-15 19:38:26 +0000

    pointers: write code to pass 'withdraw insufficient funds' test

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -17,8 +20,11 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 }
 
 func (w *Wallet) Withdraw(amount Bitcoin) error {
-	w.balance -= amount
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
 
+	w.balance -= amount
 	return nil
 }
 
