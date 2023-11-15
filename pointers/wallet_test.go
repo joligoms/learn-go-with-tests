2a91ba777264160db5911c94de40c7719c76b742
João Gomes <linharesgomes.joao@gmail.com>
2023-11-15 18:53:33 +0000

    pointers: implement test with Bitcoin type

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -5,13 +5,21 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	want := 10
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		assertCorrect(t, got, want)
+	})
+}
+
+func assertCorrect(t testing.TB, got, want Bitcoin) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
