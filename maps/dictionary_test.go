249f214c825562bff27c43e1ce5aed9d1271fd86
João Gomes <linharesgomes.joao@gmail.com>
2023-11-15 21:41:58 +0000

    maps: add test for adding an existing word

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -20,10 +20,23 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		dictionary.Add("test", "this is just a test")
 
-	assertDefinition(t, dictionary, "test", "this is just a test")
+		assertDefinition(t, dictionary, "test", "this is just a test")
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
