917aad474afcbe4efc7b14f54214455cb4733450
João Gomes <linharesgomes.joao@gmail.com>
2023-11-17 23:15:39 +0000

    mocks: implement sleeper test

--- mocks/countdown.go
+++ mocks/countdown.go
@@ -5,10 +5,14 @@ import (
 	"io"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
