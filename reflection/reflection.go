2a330c9ca9e02092e9af82970b389d7243ee6467
João Gomes <linharesgomes.joao@gmail.com>
2024-02-04 11:30:57 +0000

    reflection: write enough code to make it pass

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,5 +1,9 @@
 package reflection
 
+import "reflect"
+
 func walk(x any, fn func(input string)) {
-	fn("I still can't believe Germany beat Brazil 7-1 :(")
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
 }
