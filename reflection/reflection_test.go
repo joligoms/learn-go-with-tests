c63a95c40e6d4d81d66d615b7c28efbb1e8b5416
João Gomes <linharesgomes.joao@gmail.com>
2024-02-04 16:57:45 +0000

    reflection: add array test

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -99,6 +99,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavík"},
 		},
+		{
+			"arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
 	}
 
 	for _, test := range cases {
